64e46979f78956d5efe7f9fe0f19adfb81065c6d
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-10-27 02:19:18 +0530

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,35 +1,34 @@
-let choiceBtn = document.querySelectorAll('.choice');
+let choiceBtn = document.querySelectorAll('.choiceBtn');
 let result = document.querySelector('#result');
-let play = document.querySelector('#play');
 let youChoosed = document.querySelector('#youChoosed');
 let computerChoosed = document.querySelector('#computerChoosed');
 
 Array.from(choiceBtn).forEach(element => {
     element.onclick = () => {
-        let checkboxes = document.querySelectorAll('.choice')
-        checkboxes.forEach((item) => {
-            if (item !== element) item.checked = false
-        })
+        let userChoice = element.textContent;
+        let choices = ['rock', 'paper', 'scissor'];
+        playComputer = choices[Math.floor(Math.random() * choices.length)];
+        playGame(userChoice, playComputer)
     }
-});
-
-play.addEventListener('click', () => {
-    let userChoice = document.querySelector('.choice:checked');
-    let choices = ['rock', 'paper', 'scissor'];
-    let i = Math.floor(Math.random() * choices.length)
-    playComputer = choices[i];
-    playGame(userChoice.name, playComputer)
-    userChoice.checked = false
 });
 
 function playGame(a, b) {
     youChoosed.innerText = a;
     computerChoosed.innerText = b;
-    if (a == b) result.innerText = "Game Draw !"
-    else if (userWinning(a, b)) result.innerText = "You Won !"
-    else result.innerText = "You lose !"
-
+    if (a == b) {
+        result.style.color = 'blue';
+        result.innerText = "Game Draw !"
+    }
+    else if (userWinning(a, b)) {
+        result.style.color = 'green';
+        result.innerText = "You Won !"
+    }
+    else {
+        result.innerText = "You lose !"
+        result.style.color = 'red'
+    }
 }
+
 let winningCase = {
     'rock': 'scissor',
     'paper': 'rock',
